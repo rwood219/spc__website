37c3ee84c943cb25ad8aa09326db0dd47b19fdf4
rwood219 <58787084+rwood219@users.noreply.github.com>
2022-09-08 22:15:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+//image slider sources
+const bannerImages = [
+  '/resources/accessories/pumps bells misc/wide crop bag.jpg',
+  "/resources/accessories/pumps bells misc/wide crop locks.jpg",
+  "/resources/accessories/pumps bells misc/crop mini pump.jpg"
+]
+//DOM elements being changed
+const bannerImageOne = document.querySelector('.bannerImageOne');
+const bannerImageTwo = document.querySelector('.bannerImageTwo');
+const bannerImageThree = document.querySelector('.bannerImageThree');
+//sets image src for banner image slider
+bannerImageOne.src = bannerImages[1];
+bannerImageTwo.src = bannerImages[0];
+bannerImageThree.src = bannerImages[2];
+bannerImageFour.src = bannerImages[3];
+
+//sun bike image sources
+const sunDrifterImg = [];
+
+//pure bike image sources
+const pureBikeImg = [];
+
