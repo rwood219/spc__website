942179d14dc7deae45b9e5d693687c51cef68ca4
rwood219 <58787084+rwood219@users.noreply.github.com>
2022-09-12 23:48:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,16 +4,12 @@ const bannerImages = [
   '/resources/accessories/pumps bells misc/wide crop bag.jpg',
   "/resources/accessories/pumps bells misc/wide crop locks.jpg",
   "/resources/accessories/pumps bells misc/crop mini pump.jpg"
-]
-//DOM elements being changed
-const bannerImageOne = document.querySelector('.bannerImageOne');
-const bannerImageTwo = document.querySelector('.bannerImageTwo');
-const bannerImageThree = document.querySelector('.bannerImageThree');
-//sets image src for banner image slider
-bannerImageOne.src = bannerImages[1];
-bannerImageTwo.src = bannerImages[0];
-bannerImageThree.src = bannerImages[2];
-bannerImageFour.src = bannerImages[3];
+];
+
+//SET BANNER IMAGES
+document.querySelector('.bannerImageOne').src = bannerImages[1];
+document.querySelector('.bannerImageTwo').src = bannerImages[0];
+document.querySelector('.bannerImageThree').src = bannerImages[2];
 
 //sun bike image sources
 const sunDrifterImg = [];
@@ -21,6 +17,17 @@ const sunDrifterImg = [];
 //pure bike image sources
 const pureBikeImg = [];
 
+//SET STORE HOURS
+$('.sunday-hours').html('SUNDAY 10-5');
+$('.monday-hours').html('MONDAY 10-7');
+$('.tueday-hours').html('TUESDAY 10-7');
+$('.wednesday-hours').html('WEDNESDAY 10-7');
+$('.thursday-hours').html('THURSDAY 10-7');
+$('.friday-hours').html('FRIDAY 10-7');
+$('.saturday-hours').html('SATURDAY 9-7');
+
+//set phone number
+$('.phone-number').html('(609) 365 - 2959')
 
 
-
+console.log('conn')
