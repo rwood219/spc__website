2af0af8dbdd05867d0b7b257e5d5b5b007726e37
rwood219 <58787084+rwood219@users.noreply.github.com>
2022-12-21 09:56:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,12 @@
 const bannerImages = [
   '/resources/accessories/pumps bells misc/wide crop bag.jpg',
   "/resources/accessories/pumps bells misc/wide crop locks.jpg",
-  "/resources/accessories/pumps bells misc/crop mini pump.jpg"
+  "/resources/accessories/pumps bells misc/crop mini pump.jpg",
+  "/resources/engwee/engine-pro.jpg"
 ];
 
 //SET BANNER IMAGES
-document.querySelector('.bannerImageOne').src = bannerImages[1];
+document.querySelector('.bannerImageOne').src = bannerImages[3];
 document.querySelector('.bannerImageTwo').src = bannerImages[0];
 document.querySelector('.bannerImageThree').src = bannerImages[2];
 
@@ -19,12 +20,12 @@ const pureBikeImg = [];
 
 //SET STORE HOURS
 $('.sunday-hours').html('SUNDAY 10-5');
-$('.monday-hours').html('MONDAY 10-7');
-$('.tueday-hours').html('TUESDAY 10-7');
-$('.wednesday-hours').html('WEDNESDAY 10-7');
-$('.thursday-hours').html('THURSDAY 10-7');
-$('.friday-hours').html('FRIDAY 10-7');
-$('.saturday-hours').html('SATURDAY 9-7');
+$('.monday-hours').html('MONDAY 10-6');
+$('.tuesday-hours').html('TUESDAY 10-6');
+$('.wednesday-hours').html('WEDNESDAY 10-6');
+$('.thursday-hours').html('THURSDAY 10-6');
+$('.friday-hours').html('FRIDAY 10-6');
+$('.saturday-hours').html('SATURDAY 10-7');
 
 //set phone number
 $('.phone-number').html('(609) 365 - 2959')
