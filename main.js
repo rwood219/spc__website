7631392df426cc875a6be4cb23b2f32937004184
rwood219 <58787084+rwood219@users.noreply.github.com>
2023-01-21 11:12:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
 
 //image slider sources
 const bannerImages = [
-  '/resources/accessories/pumps bells misc/wide crop bag.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReEMiOEVmqytULT7a-0yVqlVMFlLiZhJcGwQ&usqp=CAU',
   "/resources/accessories/pumps bells misc/wide crop locks.jpg",
   "/resources/accessories/pumps bells misc/crop mini pump.jpg",
   "/resources/engwee/engine-pro.jpg"
 ];
 
 //SET BANNER IMAGES
-document.querySelector('.bannerImageOne').src = bannerImages[3];
-document.querySelector('.bannerImageTwo').src = bannerImages[0];
-document.querySelector('.bannerImageThree').src = bannerImages[2];
+document.querySelector('.bannerImageOne').src = 'https://productimages.qbp.com/6SPsvm45/prodxl/BK9540.jpg';
+document.querySelector('.bannerImageTwo').src = 'https://images.jbi.bike/2200/670731.jpg';
+document.querySelector('.bannerImageThree').src = 'https://electricbikecompany.com/wp-content/uploads/2019/05/home-model-c-VIN-SGR.jpg';
 
 //sun bike image sources
-const sunDrifterImg = [];
+const sunDrifterImg = [
+  'https://images.jbi.bike/2200/670731.jpg',
+  'https://images.jbi.bike/2200/670732.jpg',
+
+];
 
 //pure bike image sources
 const pureBikeImg = [];
 
 //SET STORE HOURS
-$('.sunday-hours').html('SUNDAY 10-5');
-$('.monday-hours').html('MONDAY 10-6');
-$('.tuesday-hours').html('TUESDAY 10-6');
-$('.wednesday-hours').html('WEDNESDAY 10-6');
-$('.thursday-hours').html('THURSDAY 10-6');
-$('.friday-hours').html('FRIDAY 10-6');
-$('.saturday-hours').html('SATURDAY 10-7');
+
+
+$('.sunday-hours').html('SUNDAY CLOSED');
+$('.monday-hours').html('MONDAY CLOSED');
+$('.tuesday-hours').html('TUESDAY CLOSED');
+$('.wednesday-hours').html('WEDNESDAY 10-5');
+$('.thursday-hours').html('THURSDAY 10-5');
+$('.friday-hours').html('FRIDAY 10-5');
+$('.saturday-hours').html('SATURDAY 10-5');
 
 //set phone number
 $('.phone-number').html('(609) 365 - 2959')
+
+// add winter hours div
+
+document.querySelector('.hours').appendChild(document.createTextNode("WINNTER HOURS"));
+
 
 
 console.log('conn')
